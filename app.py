58fdf21c394f242b30bdfc1c217a66804a48be50
Krzysztof Mola <91846431+KrzysztofiniOWO@users.noreply.github.com>
2024-02-19 21:32:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
 import requests
 import time
-import socket
-import socks
-from stem import Signal
-from stem.control import Controller
 
 # Adres SOCKS proxy dla lokalnej instancji Tor
 TOR_SOCKS_PORT = 9050
 TOR_CONTROL_PORT = 9051
 
-# Funkcja do zmiany adresu IP przez bezpośredni SOCKS proxy
-def change_tor_circuit():
-    try:
-        with Controller.from_port(port=TOR_CONTROL_PORT) as controller:
-            controller.authenticate()  # Uwierzytelnianie przy użyciu kontroli Tor
-            controller.signal(Signal.NEWNYM)  # Wysyłanie sygnału do zmiany obwodu
-            print("New Tor circuit created.")
-    except Exception as e:
-        print(f"Error: {e}")
-        print("Unable to change Tor circuit.")
-
 # Funkcja do wykonania zapytania HTTP przez Tor
-def make_tor_request(url, max_content_lines=10):
+def make_tor_request(url):
     session = requests.session()
     session.proxies = {'http': f'socks5h://localhost:{TOR_SOCKS_PORT}',
                        'https': f'socks5h://localhost:{TOR_SOCKS_PORT}'}
@@ -42,11 +27,6 @@ def make_tor_request(url, max_content_lines=10):
             print(f"Peer IP Address: {peer_ip}")
         except AttributeError:
             print("Unable to retrieve IP Address.")
-
-        # Wyłączono wydruk treści odpowiedzi
-        # content_lines = response.text.split('\n')[:max_content_lines]
-        # content_preview = '\n'.join(content_lines)
-        # print(f"Content Preview:\n{content_preview}")
 
         with open('results.txt', 'a') as file:
             file.write(f"Success - Status Code: {response.status_code}, Elapsed Time: {elapsed_time:.2f}, Local IP Address: {local_ip if 'local_ip' in locals() else 'N/A'}, Peer IP Address: {peer_ip if 'peer_ip' in locals() else 'N/A'}, URL: {url}\n")
@@ -71,7 +51,6 @@ if __name__ == "__main__":
     try:
         num_iterations = int(input("Podaj liczbę iteracji (zmian adresu IP): "))
         for _ in range(num_iterations):
-            change_tor_circuit()
             time.sleep(5)
             make_tor_request("https://www.nytimesn7cgmftshazwhfgzm37qxb44r64ytbb2dj3x62d2lljsciiyd.onion/")
 

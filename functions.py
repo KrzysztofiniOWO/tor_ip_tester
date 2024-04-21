563c6460fe1fda6fc992642f65eeb3ff940a2f14
krzysztof <molakrzysztof@gmail.com>
2024-04-21 19:32:37 +0200

    Added parser to easier test further changes

--- functions.py
+++ functions.py
@@ -24,56 +24,56 @@ def make_tor_request(url, headers, proxies):
     return response, total_time
 
 
-def make_tor_requests_diff_ip(repeats):
-    for _ in range(repeats):
+def make_tor_requests_diff_ip(webpage, amount, path):
+    for _ in range(amount):
         headers = { 'User-Agent': UserAgent().random }
         change_ip()
-        response, total_time = make_tor_request("http://httpbin.org/ip", headers, proxies)
-        ip_address = response.json()["origin"]
-        result = f"IP Address: {ip_address}, " + f"{total_time}, " + f"{response.status_code}\n"
-        name = "results_diff_ip.txt"
+        response, total_time = make_tor_request(webpage, headers, proxies)
+        my_ip = requests.get("http://httpbin.org/ip", headers=headers, proxies=proxies).json()["origin"]
+        result = f"IP Address: {my_ip}, " + f"{total_time}, " + f"{response.status_code}" + f" {webpage}\n"
+        name = f"{path}/results_diff_ip.txt"
         save_requests_results(result, name)
 
 
-def make_tor_requests_same_ip(repeats):
-    for _ in range(repeats):
+def make_tor_requests_same_ip(webpage, amount, path):
+    for _ in range(amount):
         headers = {'User-Agent': UserAgent().random}
-        response, total_time = make_tor_request("http://httpbin.org/ip", headers, proxies)
-        ip_address = response.json()["origin"]
-        result = f"IP Address: {ip_address}, " + f"{total_time}, " + f"{response.status_code}\n"
-        name = "results_same_ip.txt"
+        response, total_time = make_tor_request(webpage, headers, proxies)
+        my_ip = requests.get("http://httpbin.org/ip", headers=headers, proxies=proxies).json()["origin"]
+        result = f"IP Address: {my_ip}, " + f"{total_time}, " + f"{response.status_code}" + f" {webpage}\n"
+        name = f"{path}/results_same_ip.txt"
         save_requests_results(result, name)
 
 
-def check_first_image_load_time_diff_ip(repeats):
-    for repeat in range(repeats):
+def check_first_image_load_time_diff_ip(webpage, amount, path):
+    for repeat in range(amount):
         headers = {'User-Agent': UserAgent().random}
         change_ip()
-        response, _ = make_tor_request("https://www.wp.pl/", headers, proxies)
+        response, _ = make_tor_request(webpage, headers, proxies)
         soup = bs(response.content, 'html.parser')
         first_image = soup.find('img')
         if first_image:
             image_url = first_image['src']
             _, total_time = make_tor_request(image_url, headers, proxies)
             print(f"image load time diff ip {total_time}")
-            location = f"images/img_diff_ip_{repeat}.jpg"
+            location = f"{path}/img_diff_ip_{repeat}.jpg"
             save_image(response.content, location)
             print(total_time)
         else:
             print("Image could not be found")
 
 
-def check_first_image_load_time_same_ip(repeats):
-    for repeat in range(repeats):
+def check_first_image_load_time_same_ip(webpage, amount, path):
+    for repeat in range(amount):
         headers = {'User-Agent': UserAgent().random}
-        response, _ = make_tor_request("https://www.wp.pl/", headers, proxies)
+        response, _ = make_tor_request(webpage, headers, proxies)
         soup = bs(response.content, 'html.parser')
         first_image = soup.find('img')
         if first_image:
             image_url = first_image['src']
             _, total_time = make_tor_request(image_url, headers, proxies)
             print(f"image load time same ip {total_time}")
-            location = f"images/img_same_ip_{repeat}.jpg"
+            location = f"{path}/img_same_ip_{repeat}.jpg"
             save_image(response.content, location)
             print(total_time)
         else:

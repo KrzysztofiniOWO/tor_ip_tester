3dee3555a38d53f9c77869a3f6322315c946e56a
krzysztof <molakrzysztof@gmail.com>
2024-05-26 15:10:42 +0200

    Added tests for mongo db need to update parser

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import time
 import requests
+from pymongo import MongoClient
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
 from stem import Signal
@@ -22,6 +23,18 @@ def make_tor_request(url, headers, proxies):
     end_time = time.time()
     total_time = end_time - start_time
     return response, total_time
+
+def test_mongodb_query(uri, dbname, collection_name, query):
+    client = MongoClient(uri)
+    db = client[dbname]
+    collection = db[collection_name]
+    start_time = time.time()
+    result = collection.find_one(query)
+    end_time = time.time()
+    total_time = end_time - start_time
+    client.close()
+    return total_time
+
 
 
 def make_tor_requests_diff_ip(webpage, amount, path):
@@ -78,8 +91,24 @@ def check_first_image_load_time_same_ip(webpage, amount, path):
             print(total_time)
         else:
             print("Image could not be found")
-        
 
+def test_mongodb_diff_ip(amount, path, db_config, query):
+    for _ in range(amount):
+        change_ip()
+        total_time = test_mongodb_query(db_config['uri'], db_config['dbname'], db_config['collection'], query)
+        my_ip = requests.get("http://httpbin.org/ip", proxies=proxies).json()["origin"]
+        result = f"IP Address: {my_ip}, Query Time: {total_time}s\n"
+        name = f"{path}/results/mongodb_results_diff_ip.txt"
+        save_requests_results(result, name)
+
+def test_mongodb_same_ip(amount, path, db_config, query):
+    for _ in range(amount):
+        total_time = test_mongodb_query(db_config['uri'], db_config['dbname'], db_config['collection'], query)
+        my_ip = requests.get("http://httpbin.org/ip", proxies=proxies).json()["origin"]
+        result = f"IP Address: {my_ip}, Query Time: {total_time}s\n"
+        name = f"{path}/results/mongodb_results_same_ip.txt"
+        save_requests_results(result, name)
+        
 def save_requests_results(result, name):
     with open(name, 'a') as file:
         file.write(result)
--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import functions
 import calculations
 
-repeats = int(input("How many requests you want to make?: "))
+db_config = {
+    'uri': 'mongodb+srv://molakrzysztof:<pass>@cluster0.x1x8qhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',  
+    'dbname': 'inzynierka',
+    'collection': 'sample_airbnb'
+}
+query = {"property_type": "house"}
 
-functions.make_tor_requests_diff_ip(repeats)
-functions.make_tor_requests_same_ip(repeats)
+#repeats = int(input("How many requests you want to make?: "))
 
-img_load_time = functions.check_first_image_load_time_diff_ip(repeats)
-img_load_time = functions.check_first_image_load_time_same_ip(repeats)
+# functions.make_tor_requests_diff_ip(repeats)
+# functions.make_tor_requests_same_ip(repeats)
 
-calculations.mean_of_requests_time("results_diff_ip.txt")
-calculations.mean_of_requests_time("results_same_ip.txt")
+# img_load_time = functions.check_first_image_load_time_diff_ip(repeats)
+# img_load_time = functions.check_first_image_load_time_same_ip(repeats)
+
+# calculations.mean_of_requests_time("results_diff_ip.txt")
+# calculations.mean_of_requests_time("results_same_ip.txt")
+
+functions.test_mongodb_diff_ip(3, '/home/krzysztof/tor_ip_tester', db_config, query)
+functions.test_mongodb_same_ip(3, '/home/krzysztof/tor_ip_tester', db_config, query)

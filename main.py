9dcb31d78e4e4d759f6aa31bacb02c92c7e6f36c
krzysztof <molakrzysztof@gmail.com>
2024-08-03 21:12:09 +0200

    Move some functions to utils

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import calculations
 
 results_path = '/home/krzysztof/tor_ip_tester/results'
 downloads_path = '/home/krzysztof/tor_ip_tester/downloads'
+test_data_path = '/home/krzysztof/tor_ip_tester/test_data'
 
 db_config = {
     'uri': 'mongodb+srv://<pass>@cluster0.x1x8qhv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',  
@@ -13,26 +14,26 @@ query = {"property_type": "house"}
 
 repeats = int(input("How many requests you want to make?: "))
 
-# functions.make_tor_requests_diff_ip('https://wp.pl', repeats, results_path)
-# functions.make_tor_requests_same_ip('https://wp.pl', repeats, results_path)
+functions.make_tor_requests_diff_ip('https://wp.pl', repeats, results_path)
+functions.make_tor_requests_same_ip('https://wp.pl', repeats, results_path)
 
-# functions.check_first_image_download_time_diff_ip('https://wp.pl', repeats, results_path, downloads_path)
-# functions.check_first_image_download_time_same_ip('https://wp.pl', repeats, results_path, downloads_path)
+functions.check_first_image_download_time_diff_ip('https://wp.pl', repeats, results_path, downloads_path)
+functions.check_first_image_download_time_same_ip('https://wp.pl', repeats, results_path, downloads_path)
 
-# functions.test_mongodb_diff_ip(repeats, results_path, db_config, query)
-# functions.test_mongodb_same_ip(repeats, results_path, db_config, query)
+functions.test_mongodb_diff_ip(repeats, results_path, db_config, query)
+functions.test_mongodb_same_ip(repeats, results_path, db_config, query)
 
-# functions.download_file_diff_ip("https://sample-videos.com/download-sample-text-file.php", repeats, results_path, downloads_path)
-# functions.download_file_same_ip("https://sample-videos.com/download-sample-text-file.php", repeats, results_path, downloads_path)
+functions.download_file_diff_ip("https://sample-videos.com/download-sample-text-file.php", repeats, results_path, downloads_path)
+functions.download_file_same_ip("https://sample-videos.com/download-sample-text-file.php", repeats, results_path, downloads_path)
 
-# functions.test_upload_file_ftp_diff_ip('eu-west-1.sftpcloud.io', 'f831873852d84bb6b0c82e0f6549fd1b', 'vzy3qOglaqIZsmhzFrHtjAhIUevaKGDp', repeats, f'{downloads_path}/img_diff_ip_0.jpg', results_path)
-# functions.test_upload_file_ftp_same_ip('eu-west-1.sftpcloud.io', 'f831873852d84bb6b0c82e0f6549fd1b', 'vzy3qOglaqIZsmhzFrHtjAhIUevaKGDp', repeats, f'{downloads_path}/img_diff_ip_0.jpg', results_path)
+functions.test_upload_file_ftp_diff_ip('eu-west-1.sftpcloud.io', 'f831873852d84bb6b0c82e0f6549fd1b', 'vzy3qOglaqIZsmhzFrHtjAhIUevaKGDp', repeats, f'{test_data_path}/sus.jpg', results_path)
+functions.test_upload_file_ftp_same_ip('eu-west-1.sftpcloud.io', 'f831873852d84bb6b0c82e0f6549fd1b', 'vzy3qOglaqIZsmhzFrHtjAhIUevaKGDp', repeats, f'{test_data_path}/sus.jpg', results_path)
 
-# functions.test_jsonplaceholder_get_diff_ip(repeats, results_path)
-# functions.test_jsonplaceholder_get_same_ip(repeats, results_path)
+functions.test_jsonplaceholder_get_diff_ip(repeats, results_path)
+functions.test_jsonplaceholder_get_same_ip(repeats, results_path)
 
-# functions.test_dns_resolution_diff_ip("example.com", repeats, results_path)
-# functions.test_dns_resolution_same_ip("example.com", repeats, results_path)
+functions.test_dns_resolution_diff_ip("example.com", repeats, results_path)
+functions.test_dns_resolution_same_ip("example.com", repeats, results_path)
 
-# functions.test_websocket_diff_ip("wss://echo.websocket.org", repeats, results_path)
-# functions.test_websocket_same_ip("wss://echo.websocket.org", repeats, results_path)
+functions.test_websocket_diff_ip("wss://echo.websocket.org", repeats, results_path)
+functions.test_websocket_same_ip("wss://echo.websocket.org", repeats, results_path)
---
+++ utils.py
@@ -0,0 +1,59 @@
+import time
+from stem.control import Controller
+from stem import Signal
+import requests
+from pymongo import MongoClient
+import ftplib
+import websockets
+
+def change_ip():
+    time.sleep(10)
+    with Controller.from_port(port=9051) as c:
+        c.authenticate()
+        c.signal(Signal.NEWNYM)
+
+def test_mongodb_query(uri, dbname, collection_name, query):
+    client = MongoClient(uri)
+    db = client[dbname]
+    collection = db[collection_name]
+    start_time = time.time()
+    result = collection.find_one(query)
+    end_time = time.time()
+    total_time = end_time - start_time
+    client.close()
+    return total_time
+
+def upload_file_ftp(server, username, password, file_path):
+    try:
+        ftp = ftplib.FTP(server)
+        ftp.login(username, password)
+        with open(file_path, 'rb') as file:
+            start_time = time.time()
+            ftp.storbinary(f'STOR {file_path}', file)
+            end_time = time.time()
+        ftp.quit()
+        return end_time - start_time
+    except ftplib.all_errors as e:
+        print(f"FTP error: {e}")
+        return None
+    
+def fetch_post(api_url):
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            json_data = response.json()
+            return json_data
+        else:
+            print(f"Failed to fetch data. Status code: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+    
+async def websocket_test(uri):
+    async with websockets.connect(uri) as websocket:
+        start_time = time.perf_counter()
+        await websocket.send("Hello, WebSocket!")
+        response = await websocket.recv()
+        end_time = time.perf_counter()
+        return end_time - start_time, response

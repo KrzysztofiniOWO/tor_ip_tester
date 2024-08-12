b4dc5f024d395f16f4ed91ed18b9004705028377
krzysztof <molakrzysztof@gmail.com>
2024-08-12 21:30:54 +0200

    Calculations working fine

--- calculations.py
+++ calculations.py
@@ -1,22 +1,26 @@
-def calculate_mean_time(file_path):
+import numpy as np
+
+def calculate_mean_time(file_name):
+    file_path = f"results/{file_name}"
+
     time_values = []
 
-    with open(file_path, 'r') as f:
-        for line in f:
+    with open(file_path, 'r') as fr:
+        for line in fr:
             parts = line.strip().split(', ')
-            if len(parts) >= 2:
-                try:
-                    time_value = float(parts[1])
-                    time_values.append(time_value)
-                except ValueError:
-                    continue
+            time_value = float(parts[1].rstrip(','))
+            time_values.append(time_value)
+    fr.close()
 
-    f.close()
-    if time_values:
-        mean_time = sum(time_values) / len(time_values)
-        with open(f'statistics/statistics_'+file_path, 'w') as output_file:
-            output_file.write(f"Mean time: {mean_time}\n")
-        print('kurwa')
-        return mean_time
-    else:
-        return None
+    time_mean = sum(time_values) / len(time_values)
+    time_median = np.median(time_values)
+    time_min = min(time_values)
+    time_max = max(time_values)
+
+    with open(f'statistics/statistics_{file_name}', 'w')as fw:
+        fw.write(f"Mean of times: {time_mean}\n")
+        fw.write(f"Median of times: {time_median}\n")
+        fw.write(f"Min of times: {time_min}\n")
+        fw.write(f"Max of times: {time_max}\n")
+    fw.close()
+
